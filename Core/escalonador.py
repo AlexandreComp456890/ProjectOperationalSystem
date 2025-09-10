48a9ed13a74ebb776b54fab77144e58bb959a89f
YanSilva22 <yanviniciusjeba9@gmail.com>
2025-09-10 14:09:11 -0300

    Implementa classe Escalonador
    Atributos: fila_de_prontos e processo_atual
    Getters e Setters encapsulados com @property
    Métodos base: EscolherProcesso e AlternarProcesso
    Estrutura inicial para algoritmos de escalonamento (SJF, Ruby_Ribbon, Prioridade)

---
+++ Core/escalonador.py
@@ -0,0 +1,37 @@
+from typing import List
+from processo import Processo  # Importa a classe
+
+class Escalonador:
+    def __init__(self, FilaDeProntos: List[Processo], ProcessoAtual: Processo):
+        self.__fila_de_prontos: List[Processo] = FilaDeProntos  # Fila de processos prontos para execução
+        self.__processo_atual: Processo = ProcessoAtual         # Processo atualmente em execução
+
+    # GETTERS
+    @property
+    def fila_de_prontos(self) -> List[Processo]:
+        return self.__fila_de_prontos
+    @property
+    def processo_atual(self) -> Processo:
+        return self.__processo_atual
+    
+    # SETTERS
+    @fila_de_prontos.setter
+    def fila_de_prontos(self, nova_fila: List[Processo]):   
+        self.__fila_de_prontos = nova_fila
+    @processo_atual.setter
+    def processo_atual(self, novo_processo: Processo):  
+        self.__processo_atual = novo_processo
+
+    # Métodos
+    def EscolherProcesso(self):
+        pass
+    def AlternarProcesso(self):
+        pass
+
+    # Algoritmos de Escalonamento
+    def SJF(self):
+        pass
+    def Ruby_Ribbon(self):
+        pass
+    def Prioridade(self):
+        pass
