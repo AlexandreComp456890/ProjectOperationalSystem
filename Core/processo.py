f2e9c2206bc7efadb0d55006f8dfec6acfc914e9
AlexandreComp456890 <alexandre.rocha.fonte@gmail.com>
2025-09-15 13:47:45 -0300

    Modificando processos.py: - Adição de Enum para controle de estados do processo.

--- Core/processo.py
+++ Core/processo.py
@@ -1,14 +1,22 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import List
-from threading_processo import Threading # Importa a classe
+from thread import Thread # Importa a classe
+
+class Estados(Enum):
+    NEW = "Novo"
+    READY = "Pronto"
+    BLOCKED = "Bloqueado"
+    EXECUTED = "Executado"
+    FINISHED = "Terminado"
 
 class Processo(ABC):
-    def __init__(self, IdProcesso: str, Estado: str, Prioridade: int, Tempo_Exec: int, ThreadingFilhas: List[Threading]):
+    def __init__(self, IdProcesso: str, Prioridade: int, Tempo_Exec: int):
         self.__id_processo: str = IdProcesso       # Identificador do processo
-        self.__estado: str = Estado                 # Estado: pronto, executando, bloqueado, finalizado
+        self.__estado: Estados = Estados.NEW                # Estado: novo, pronto, executando, bloqueado, finalizado
         self.__prioridade: int = Prioridade         # Prioridade do processo
         self.__tempo_exec: int = Tempo_Exec         # Tempo de execução em unidades (ex: segundos ou quantum)
-        self.__threads_filhas: List[Threading] = ThreadingFilhas  # Threads filhas do processo
+        self.__threads_filhas: List[Thread] = []  # Threads filhas do processo
 
     # GETTERS
 
@@ -19,13 +27,13 @@ class Processo(ABC):
     def tempo_exec(self) -> int:
         return self.__tempo_exec
     @property
-    def estado(self) -> str:
+    def estado(self) -> Estados:
         return self.__estado
     @property
     def prioridade(self) -> int:
         return self.__prioridade
     @property
-    def threads_filhas(self) -> List[Threading]:
+    def threads_filhas(self) -> List[Thread]:
         return self.__threads_filhas
 
     # SETTERS
@@ -36,13 +44,13 @@ class Processo(ABC):
     def tempo_exec(self, novo_tempo: int):
         self.__tempo_exec = novo_tempo
     @estado.setter
-    def estado(self, novo_estado: str):
+    def estado(self, novo_estado: Estados):
         self.__estado = novo_estado
     @prioridade.setter
     def prioridade(self, nova_prioridade: int):
         self.__prioridade = nova_prioridade
     @threads_filhas.setter
-    def threads_filhas(self, novas_threads: List[Threading]):
+    def threads_filhas(self, novas_threads: List[Thread]):
         self.__threads_filhas = novas_threads
 
     # Métodos abstratos
