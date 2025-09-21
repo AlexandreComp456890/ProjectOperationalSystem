50ac3865982075a5d9f449058ce9c1adc9c2a3f6
AlexandreComp456890 <alexandre.rocha.fonte@gmail.com>
2025-09-21 15:17:17 -0300

    (refractor) Modificado thread.py - Removido propriedade EUsuario - Adicionado propriedades de controle: 	- tempo_exec controla tempo de execução da thread; 	- recurso_necessario controla o recurso que a thread pede 	com base na lista de recursos_alocados e dependencias de 	Processos.

--- Core/thread.py
+++ Core/thread.py
@@ -2,26 +2,30 @@ from typing import List
 
 class Thread:
     # constructor
-    def __init__(self, IdThread: int, EUsuario: bool):
-        self.id_thread: int = IdThread      # Identificador da thread
-        self.euusuario: bool = EUsuario     # Indica se é uma thread de usuário (True) ou de sistema (False)
+    def __init__(self, IdThread: int, Tempo_exec: int):
+        self.__id_thread: int = IdThread          # Identificador da thread
+        self.__tempo_exec: int = Tempo_exec       # Tempo de execução em unidades
+        self.__recurso_necessario: str = ""       # Recurso que a Thread precisa para executar           
 
     # GETTERS
     @property
     def id_thread(self) -> int:
         return self.__id_thread
     @property
-    def euusuario(self) -> bool:
-        return self.__euusuario
+    def tempo_exec(self) -> int:
+        return self.__tempo_exec
     
     # SETTERS
     @id_thread.setter
     def id_thread(self, novo_id: int):
         self.id_thread = novo_id
-    @euusuario.setter
-    def euusuario(self, novo_valor: bool):
-        self.euusuario = novo_valor
 
     # Métodos 
-    def Operação(self):
-        raise NotImplemented
+    def Operação(self, quantum: int) -> int:
+        if self.tempo_exec <= quantum:
+            tempo_utilizado = self.tempo_exec
+            self.tempo_exec = 0
+        else:
+            tempo_utilizado = quantum
+            self.tempo_exec -= quantum
+        return
