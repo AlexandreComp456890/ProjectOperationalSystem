503010cc791f58493576f5c3ff110e53880cf1d2
YanSilva22 <yanviniciusjeba9@gmail.com>
2025-09-21 19:32:31 -0300

    Modifica a estrutura de classes
    Modifica a estrutura da classe SistemaOperacional

--- Core/sistemaOperacional.py
+++ Core/sistemaOperacional.py
@@ -1,37 +1,66 @@
 from typing import List
 from .processo import Processo 
-from .tabelaProcessos import TabelaProcessos
+from .escalonador import Escalonador
+from .gerenciadorMemoria import GerenciadorMemoria
+from .gerenciadorRecursos import GerenciadorRecursos
+from .recurso import Recurso
 
 class SistemaOperacional:
-    def __init__(self, CPU: list[bool], Memoria: float):
-        self.__cpu: list[bool] = CPU          # Representa o estado da CPU (ocupada ou livre)
-        self.__memoria: float = Memoria        # memória disponível
+    def __init__(self, tabelaProcessos: list[Processo], escalonador: Escalonador, gerenciadorMemoria: GerenciadorMemoria, gerenciadorRecursos: GerenciadorRecursos):
+        self.__tabelaProcessos: List[Processo] = []  # Tabela de processos do sistema
+        self.__escalonador: Escalonador = escalonador               # Escalonador de processos
+        self.__gerenciador_memoria: GerenciadorMemoria = gerenciadorMemoria # Gerenciador de memória
+        self.__gerenciador_recursos: GerenciadorRecursos = gerenciadorRecursos # Gerenciador de recursos
 
     # GETTERS
     @property
-    def cpu(self) -> list[bool]:
-        return self.__cpu
     @property
-    def memoria(self) -> float:
-        return self.__memoria
-
+    def tabelaProcessos(self) -> List[Processo]:
+        return self.__tabelaProcessos
+    @property
+    def escalonador(self) -> Escalonador:
+        return self.__escalonador
+    @property
+    def gerenciador_memoria(self) -> GerenciadorMemoria:
+        return self.__gerenciador_memoria
+    @property
+    def gerenciador_recursos(self) -> GerenciadorRecursos:
+        return self.__gerenciador_recursos
+    
     # SETTERS 
-    @cpu.setter
-    def cpu(self, novo_estado: list[bool]):
-        self.__cpu = novo_estado
-    @memoria.setter
-    def memoria(self, nova_memoria: float):
-        self.__memoria = nova_memoria
+    @tabelaProcessos.setter
+    def tabelaProcessos(self, nova_tabela: List[Processo]):
+        self.__tabelaProcessos = nova_tabela
+    @escalonador.setter
+    def escalonador(self, novo_escalonador: Escalonador):
+        self.__escalonador = novo_escalonador
+    @gerenciador_memoria.setter
+    def gerenciador_memoria(self, novo_gerenciador: GerenciadorMemoria):
+        self.__gerenciador_memoria = novo_gerenciador
+    @gerenciador_recursos.setter
+    def gerenciador_recursos(self, novo_gerenciador: GerenciadorRecursos):
+        self.__gerenciador_recursos = novo_gerenciador
 
     # Métodos
 
-    def Criar(self):
-        newProcess: Processo = Processo()      
-    def Executar(self, quantum: int):
-        pass
-    def Bloquear(self):
-        pass
-    def Finalizar(self):
-        pass
-    def QuantidadeDeMemoria(self):
-        pass
+    def criarProcesso(self, pid: int, prioridade: int = 0):
+        p = Processo(pid, prioridade)
+        self.tabelaProcessos.append(p)
+        self.escalonador.adicionarProcesso(p)
+        return p 
+    
+    def finalizarProcesso(self, processo: Processo):
+        processo.terminar()
+        if processo in self.tabelaProcessos:
+            self.tabelaProcessos.remove(processo)
+
+    def escalonar(self):
+        processo = self.escalonador.obterProximoProcesso()
+        if processo:
+            processo.executar()
+
+    def alocarRecurso(self, processo: Processo, recurso: Recurso):
+        self.gerenciadorRecursos.requisitarRecurso(processo, recurso)
+
+    def liberarRecurso(self, processo: Processo, recurso: Recurso):
+        self.gerenciadorRecursos.liberarRecurso(processo, recurso)
