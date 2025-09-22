8d9864367a319160c7ac2e4a65009b35d83886cd
AlexandreComp456890 <alexandre.rocha.fonte@gmail.com>
2025-09-21 21:00:26 -0300

    (refractor) Modificado processo.py e thread.py

--- Core/processo.py
+++ Core/processo.py
@@ -1,29 +1,23 @@
-from abc import ABC, abstractmethod
 from enum import Enum
-from typing import List
+from typing import List, Self
 from .thread import Thread # Importa a classe
+from Interface.enums import EstadoProcesso 
 
-class Estados(Enum):
-    NEW = "Novo"
-    READY = "Pronto"
-    BLOCKED = "Bloqueado"
-    EXECUTED = "Executado"
-    FINISHED = "Terminado"
-
-class Processo(ABC): 
+class Processo(): 
     # Caso de processos de usuário    
     def __init__(self, IdProcesso: str, Prioridade: int, Tempo_Exec: int):
+        # atributos do processo
         self.__id_processo: str = IdProcesso        # Identificador do processo
-        self.__estado: Estados = Estados.NEW        # Estado: novo, pronto, executando, bloqueado, finalizado
+        self.__estado: EstadoProcesso = EstadoProcesso.NOVO        # Estado: novo, pronto, executando, bloqueado, finalizado
         self.__prioridade: int = Prioridade         # Prioridade do processo
         self.__tempo_exec: int = Tempo_Exec         # Tempo de execução em unidades (ex: segundos ou quantum)   
+        
+        # atributo de threads
         self.__threads_filhas: List[Thread] = []    # Threads filhas do processo
         
-        # atributos para gerar Deadlock
-        self.__recursos_alocados: List[str] = []   # Recursos atualmente alocados ao processo
+        # atributo para gerar Deadlock
         self.__dependencias: List[str] = []        # Recursos que o processo está esperando 
         
-        self.Threading()                           # Cria threads filhas
 
     # GETTERS
     @property
@@ -33,17 +27,14 @@ class Processo(ABC):
     def tempo_exec(self) -> int:
         return self.__tempo_exec
     @property
-    def estado(self) -> Estados:
-        return self.__estado
+    def estado(self) -> str:
+        return self.__estado.value
     @property
     def prioridade(self) -> int:
         return self.__prioridade
     @property
     def threads_filhas(self) -> List[Thread]:
         return self.__threads_filhas
-    @property
-    def recursos_alocados(self) -> List[str]:
-        return self.__recursos_alocados
     @property
     def dependencias(self) -> List[str]:
         return self.__dependencias
@@ -55,23 +46,23 @@ class Processo(ABC):
     @tempo_exec.setter
     def tempo_exec(self, novo_tempo: int):
         self.__tempo_exec = novo_tempo
-    @estado.setter
-    def estado(self, novo_estado: Estados):
-        self.__estado = novo_estado
     @prioridade.setter
     def prioridade(self, nova_prioridade: int):
         self.__prioridade = nova_prioridade
     @threads_filhas.setter
     def threads_filhas(self, novas_threads: List[Thread]):
         self.__threads_filhas = novas_threads
-    @recursos_alocados.setter
-    def recursos_alocados(self, novos_recursos: List[str]):
-        self.__recursos_alocados = novos_recursos
     @dependencias.setter
     def dependencias(self, novas_dependencias: List[str]):
         self.__dependencias = novas_dependencias
         
-    # Métodos abstratos
-    @abstractmethod
-    def Threading(self):
-        pass
+    # Métodos de controle de estado 
+    def Executar(self):
+        self.estado = EstadoProcesso.EXECUTANDO
+    def Bloquear(self):
+        self.estado = EstadoProcesso.BLOQUEADO
+    def Pronto(self):
+        self.estado = EstadoProcesso.PRONTO
+    def Finalizar(self):
+        self.estado = EstadoProcesso.TERMINADO
+        
--- Core/thread.py
+++ Core/thread.py
@@ -1,11 +1,14 @@
-from typing import List
-
+import random
+from .processo import Processo
+from Interface.enums import EstadoThread
+    
 class Thread:
     # constructor
-    def __init__(self, IdThread: int, Tempo_exec: int):
-        self.__id_thread: int = IdThread          # Identificador da thread
-        self.__tempo_exec: int = Tempo_exec       # Tempo de execução em unidades
-        self.__recurso_necessario: str = ""       # Recurso que a Thread precisa para executar           
+    def __init__(self, IdThread: int, tempo_exec: int, processo_pai: Processo):
+        self.__id_thread: int = IdThread                    # Identificador da thread
+        self.__estado: EstadoThread = EstadoThread.NOVA    
+        self.__tempo_exec: int = tempo_exec
+        self.__processo_pai: Processo = processo_pai    
 
     # GETTERS
     @property
@@ -14,18 +17,28 @@ class Thread:
     @property
     def tempo_exec(self) -> int:
         return self.__tempo_exec
+    @property
+    def estado(self) -> str:
+        return self.__estado.value
+    @property
+    def processo_pai(self) -> Processo:
+        return self.__processo_pai
     
     # SETTERS
     @id_thread.setter
     def id_thread(self, novo_id: int):
         self.id_thread = novo_id
+    @tempo_exec.setter
+    def tempo_exec(self, novo_tempo: int = None):
+        if ((novo_tempo is not None) and (novo_tempo > 0)):
+            self.__tempo_exec = novo_tempo
+            return
+        self.__tempo_exec = random.randint(1, 10)
 
     # Métodos 
-    def Operação(self, quantum: int) -> int:
-        if self.tempo_exec <= quantum:
-            tempo_utilizado = self.tempo_exec
-            self.tempo_exec = 0
-        else:
-            tempo_utilizado = quantum
-            self.tempo_exec -= quantum
-        return
+    def Executar(self):
+        self.__estado = EstadoThread.EXECUTANDO
+        
+    def Liberar(self):
+        self.__estado = EstadoThread.BLOQUEADA
+        
